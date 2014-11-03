98862fdf0705f9fde49c025b3a070849a8ed2564
foostan <ks@fstn.jp>
2014-11-03 21:55:48 +0900

    Use logger

--- photolog/executor_test.go
+++ photolog/executor_test.go
@@ -6,17 +6,22 @@ import (
 )
 
 type TestExecutor struct {
+	logger *log.Logger
 }
 
 func (e *TestExecutor) Run(file_path string) error {
-	log.SetLevel(log.ErrorLevel)
-	log.Info(file_path)
+	e.logger.Level = log.ErrorLevel
+	e.logger.Info(file_path)
 
 	return nil
 }
 
 func TestDirExec(t *testing.T) {
-	err := DirExec("../test/resources/photos", &TestExecutor{})
+	testExecuter := &TestExecutor{
+		logger: log.New(),
+	}
+
+	err := DirExec("../test/resources/photos", testExecuter)
 	if err != nil {
 		t.Errorf("err: %v", err)
 	}
