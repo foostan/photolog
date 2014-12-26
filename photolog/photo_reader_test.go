5fa48d95f615203d841d617c0915c1edcece44d9
foostan <ks@fstn.jp>
2014-12-27 01:31:54 +0900

    Fix test resources

--- photolog/photo_reader_test.go
+++ photolog/photo_reader_test.go
@@ -13,7 +13,7 @@ func TestRead(t *testing.T) {
 		logger: logger,
 	}
 
-	_, err := r.Read("../test/resources/photos/2014/07/19/sample01.jpg")
+	_, err := r.Read("../test/resources/photos/2014/07/19/1405728000b5d4582798f000c70a0e97454dd63f5d.jpg")
 	if err != nil {
 		t.Errorf("err: %v", err)
 	}
@@ -21,12 +21,12 @@ func TestRead(t *testing.T) {
 		t.Errorf("err: %v", err)
 	}
 
-	_, err = r.Read("../test/resources/photos/2013/11/06/sample02.jpg")
+	_, err = r.Read("../test/resources/photos/2013/11/06/1383749906675e1fa8d8915b256f79601adca1e74e.jpg")
 	if err != nil {
 		t.Errorf("err: %v", err)
 	}
 
-	_, err = r.Read("../test/resources/photos/2013/11/23/sample03.jpg")
+	_, err = r.Read("../test/resources/photos/2013/11/23/13851841026d550cf064aea2c866fc64314257d39b.jpg")
 	if err != nil {
 		t.Errorf("err: %v", err)
 	}
