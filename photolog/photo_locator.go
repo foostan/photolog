357e0ec0febd3a56d465adaad54964afae2ab612
foostan <ks@fstn.jp>
2014-12-27 23:47:12 +0900

    Remove empty directories

--- photolog/photo_locator.go
+++ photolog/photo_locator.go
@@ -3,9 +3,9 @@ package photolog
 import (
 	"fmt"
 	log "github.com/Sirupsen/logrus"
-	"strings"
 	"os"
 	"path/filepath"
+	"io/ioutil"
 )
 
 type PhotoLocator struct {
@@ -27,20 +27,25 @@ func (e *PhotoLocator) Run(path string) error {
 		return nil
 	}
 
-	location, err := e.getLocation(pi)
+	photoLocation, err := e.getLocation(pi)
 	if err != nil {
 		return err
 	}
 
-	if path != location {
-		e.Logger.Info("Rename to " + location)
+	if path != photoLocation {
+		e.Logger.Info("Rename to " + photoLocation)
 
-		err = os.MkdirAll(filepath.Dir(location), os.FileMode(0755))
+		err = os.MkdirAll(filepath.Dir(photoLocation), os.FileMode(0755))
 		if err != nil {
-			return fmt.Errorf("Create directories of '%s' : %s", location, err)
+			return fmt.Errorf("Create directories of '%s' : %s", photoLocation, err)
 		}
 
-		err = os.Rename(path, location)
+		err = os.Rename(path, photoLocation)
+		if err != nil {
+			return err
+		}
+
+		err = removeAllEmpDir(filepath.Dir(path))
 		if err != nil {
 			return err
 		}
@@ -58,5 +63,23 @@ func (e *PhotoLocator) getLocation(pi *PhotoInfo) (string, error) {
 		return "", err
 	}
 
-	return strings.Join([]string{e.BasePath, year, month, day, name}, "/"), nil
+	return filepath.Join(e.BasePath, year, month, day, name), nil
+}
+
+func removeAllEmpDir(path string) error {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return err
+	}
+
+	if len(files) == 0 {
+		err := os.Remove(path)
+		if err != nil {
+			return fmt.Errorf("Error while removing '%s' : %s", path, err)
+		}
+
+		return removeAllEmpDir(filepath.Dir(path))
+	}
+
+	return nil
 }
