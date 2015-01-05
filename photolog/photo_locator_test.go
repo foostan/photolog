55ea8affef0d9ce2f7e1790bead3568c062d0a1e
foostan <ks@fstn.jp>
2015-01-06 00:56:02 +0900

    Fix test of photolocater

--- photolog/photo_locator_test.go
+++ photolog/photo_locator_test.go
@@ -1,17 +1,18 @@
 package photolog
 
 import (
-	"testing"
 	log "github.com/Sirupsen/logrus"
+	"testing"
 )
 
-func TestExplore(t *testing.T) {
+func TestPhotoLocator(t *testing.T) {
 	logger := log.New()
 	logger.Level = log.ErrorLevel
 	srcDir := "../test/resources/photos"
 	dstDir := srcDir
+	mode := "move"
 
-	err := DirExec(srcDir, NewPhotoLocator(srcDir, dstDir, logger))
+	err := DirExec(srcDir, NewPhotoLocator(srcDir, dstDir, mode, logger))
 	if err != nil {
 		t.Errorf("err: %v", err)
 	}
