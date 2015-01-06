eee03b492b70ed2c999f60da4e1f0167a070e350
foostan <ks@fstn.jp>
2015-01-07 01:39:23 +0900

    Limit goroutine

--- photolog/executor.go
+++ photolog/executor.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 )
 
 type Executor interface {
@@ -11,39 +12,50 @@ type Executor interface {
 }
 
 func DirExec(path string, executor Executor) error {
-	f, err := os.Open(path)
-	if err != nil {
-		return fmt.Errorf("Read '%s': %s", path, err)
-	}
-	defer f.Close()
+	cpus := runtime.NumCPU()
+	runtime.GOMAXPROCS(cpus)
 
-	fi, err := f.Stat()
-	if err != nil {
-		return fmt.Errorf("Read '%s': %s", path, err)
-	}
+	cnt := 0
+	paths := []string{path}
+	errChan := make(chan error)
+	semaphore := make(chan int, cpus)
+	for len(paths) > 0 {
+		path, paths = paths[len(paths)-1], paths[:len(paths)-1]
 
-	if fi.IsDir() {
-		contents, err := f.Readdir(-1)
+		f, err := os.Open(path)
+		if err != nil {
+			return fmt.Errorf("Read '%s': %s", path, err)
+		}
+		defer f.Close()
+
+		fi, err := f.Stat()
 		if err != nil {
 			return fmt.Errorf("Read '%s': %s", path, err)
 		}
 
-		errChan := make(chan error)
-		for _, fi := range contents {
-			subpath := filepath.Join(path, fi.Name())
-			go func(subpath string) {
-				errChan <- DirExec(subpath, executor)
-			}(subpath)
-		}
-		for i := 0; i < len(contents); i++ {
-			if <-errChan != nil {
-				return err
+		if fi.IsDir() {
+			contents, err := f.Readdir(-1)
+			if err != nil {
+				return fmt.Errorf("Read '%s': %s", path, err)
 			}
+
+			for _, fi := range contents {
+				subpath := filepath.Join(path, fi.Name())
+				paths = append(paths, subpath)
+			}
+		} else {
+			cnt++
+			go func(path string) {
+				semaphore <- 1
+				errChan <- executor.Run(path)
+				<-semaphore
+			}(path)
 		}
-	} else {
-		err := executor.Run(path)
+	}
+	for i := 0; i < cnt; i++ {
+		err := <-errChan
 		if err != nil {
-			return fmt.Errorf("Run at '%s': %s", path, err)
+			return err
 		}
 	}
 
